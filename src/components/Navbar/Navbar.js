39a194cdfd3ee2960a9c5fbc2b51836cf6d838c8
lucasbanksys <lucs@live.jp>
2021-11-25 17:38:44 -0300

    criação do card e do container

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,47 +1,56 @@
-import './Navbar.css';
-import logo from './logo.png';
-import { FaHeart, FaRegQuestionCircle, FaRegUser, FaUserPlus, FaGamepad } from 'react-icons/fa';
+import "./Navbar.css";
+import logo from "./logo.png";
+import {
+  FaHeart,
+  FaRegQuestionCircle,
+  FaRegUser,
+  FaUserPlus,
+  FaGamepad,
+} from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
-      <a className='navbar__logo' href='/'>
-        <img className='navbar__image' src={logo} alt='Nintendo Shop'></img>
+    <div className="navbar">
+      <a className="navbar__logo" href="/">
+        <img className="navbar__image" src={logo} alt="AstroX Logo"></img>
       </a>
-      <div className='navbar__links'>
-        <ul className='navbar__links__list'>
+      <div className="navbar__links">
+        <ul className="navbar__links__list">
           <li>
-            <a className='items' href='https://pt-americas-support.nintendo.com/'>
+            <a
+              className="items"
+              href="https://pt-americas-support.nintendo.com/"
+            >
               <FaRegQuestionCircle />
-              <span className='text'>Suporte</span>
+              <span className="text">Suporte</span>
             </a>
           </li>
           <li>
-            <a className='items' href='/wishlist'>
+            <a className="items" href="/wishlist">
               <FaHeart />
-              <span className='text'>Lista de Desejos</span>
+              <span className="text">Lista de Desejos</span>
             </a>
           </li>
           <li>
-            <a className='items' href='/login'>
+            <a className="items" href="/login">
               <FaRegUser />
-              <span className='text'>Login</span>
+              <span className="text">Login</span>
             </a>
           </li>
           <li>
-            <a className='items' href='/register'>
+            <a className="items" href="/register">
               <FaUserPlus />
-              <span className='text'>Criar</span>
+              <span className="text">Criar</span>
             </a>
           </li>
           <li>
-            <a className='items' href='/profile'>
+            <a className="items" href="/profile">
               <FaGamepad />
-              <span className='text'>Perfil</span>
+              <span className="text">Perfil</span>
             </a>
           </li>
         </ul>
       </div>
     </div>
-  )
+  );
 }
