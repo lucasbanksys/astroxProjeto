fdf43d2e791d3afa14f0aed8b621b2daaaa32508
lucasbanksys <lucs@live.jp>
2021-11-25 19:54:33 -0300

    ajuste importacões

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import Home from "./pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import Game from "./pages/Game/Game";
+import WishList from "./pages/WishList/WishList";
+import Profile from "./pages/Profile/Profile";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 axios.defaults.baseURL = "https://nintendo-shop.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "application/json";
@@ -18,6 +22,10 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/wishlist" element={<WishList />} />
+        <Route path="login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
     <Footer />
