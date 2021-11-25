1565e1f438226a4529470d8838105d093f0d0056
lucasbanksys <lucs@live.jp>
2021-11-25 20:21:04 -0300

    implementação do useNavigate

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,54 +1,60 @@
-import './Login.css';
-import { useState } from 'react';
-import axios from 'axios';
+import "./Login.css";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
     const login = {
       email: email,
-      password: password
-    }
+      password: password,
+    };
 
-    axios.post('/auth/login', login)
-    .then(response => {
+    axios.post("/auth/login", login).then((response) => {
       const token = response.data.token;
-      localStorage.setItem('token', token)
-    })
-  }
+      localStorage.setItem("token", token);
+      navigate("/");
+    });
+  };
 
   return (
-    <div className='login'>
-      <h2 className='login-title'>Conta Nintendo</h2>
-      <div className='login-container'>
-        
-        <form className='login-form' onSubmit={handleSubmit}>
-
-          <input type='text' placeholder='Endereço de e-mail' required
-            onChange={event => setEmail(event.target.value)}
+    <div className="login">
+      <h2 className="login-title">Conta Nintendo</h2>
+      <div className="login-container">
+        <form className="login-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Endereço de e-mail"
+            required
+            onChange={(event) => setEmail(event.target.value)}
           />
 
-          <input type='password' placeholder='Senha' 
-            onChange={event => setPassword(event.target.value)}
+          <input
+            type="password"
+            placeholder="Senha"
+            onChange={(event) => setPassword(event.target.value)}
           />
 
-          <div className='login-ok'>
-            <a href='!#'>
+          <div className="login-ok">
+            <a href="!#">
               <span>Esqueceu sua senha?</span>
             </a>
-            <input type='submit' value='Fazer login' />
+            <input type="submit" value="Fazer login" />
           </div>
         </form>
-        <div className='login-create'>
+        <div className="login-create">
           <span>Ainda não possui uma conta?</span>
-          <a className='btn-create' href='/register'>Criar uma conta Nintendo</a>
+          <a className="btn-create" href="/register">
+            Criar uma conta Nintendo
+          </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
