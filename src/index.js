8a5d825fc6d419993ff118a8e529f8757f6b2837
lucasbanksys <lucs@live.jp>
2021-11-25 19:09:33 -0300

    dependências

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://nintendo-shop.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 ReactDOM.render(
   <React.StrictMode>
     <Navbar />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
     <Footer />
   </React.StrictMode>,
   document.getElementById("root")
